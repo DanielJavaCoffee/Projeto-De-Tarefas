430ff20bfbed0f276b8b47181bc4334cf60948c2
daniel <danielsilvacr123@gmail.com>
2023-10-20 19:11:10 -0300

    camada service feita

---
+++ src/main/java/br/com/tarefas/service/TarefasService.java
@@ -0,0 +1,34 @@
+package br.com.tarefas.service;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+
+import br.com.tarefas.entity.Tarefa;
+import br.com.tarefas.repository.TarefasRepository;
+
+public class TarefasService {
+	
+	@Autowired
+	private TarefasRepository tarefasRepository;
+
+	public List<Tarefa> create(Tarefa tarefa){
+		tarefasRepository.save(tarefa);
+		return allList();
+	}
+	
+	public List<Tarefa> allList(){
+		Sort sort = Sort.by("prioridade").descending().and(Sort.by("nome").ascending());
+		return tarefasRepository.findAll(sort);
+	}
+	
+	public List<Tarefa> upDate(Tarefa tarefa){
+		tarefasRepository.save(tarefa);
+		return allList();
+	}
+	
+	public List<Tarefa> delete(Long id){
+		tarefasRepository.deleteById(id);
+		return allList();
+	}
+}
